12ce9c14ba02f9f9917f6a32e45e69681e9079cb
Dennis Ye <dennisye95@gmail.com>
2018-08-13 20:33:57 -0400

    sounds working

---
+++ assets/js/data.js
@@ -0,0 +1,215 @@
+var keyData = {
+  'q': 1,
+  '2': 2,
+  'w': 3,
+  '3': 4,
+  'e': 5,
+  'r': 6,
+  '5': 7,
+  't': 8,
+  '6': 9,
+  'y': 10,
+  '7': 11,
+  'u': 12,
+  'i': 13,
+  'c': 13,
+  'f': 14,
+  'v': 15,
+  'g': 16,
+  'b': 17,
+  'n': 18,
+  'j': 19,
+  'm': 20,
+  'k': 21,
+  ',': 22,
+  'l': 23,
+  '.': 24,
+  '/': 25,
+};
+
+var sounds = {
+  1: new Howl({
+    src: ['assets/sounds/key-1.wav']
+  }),
+  2: new Howl({
+    src: ['assets/sounds/key-2.wav']
+  }),
+  3: new Howl({
+    src: ['assets/sounds/key-3.wav']
+  }),
+  4: new Howl({
+    src: ['assets/sounds/key-4.wav']
+  }),
+  5: new Howl({
+    src: ['assets/sounds/key-5.wav']
+  }),
+  6: new Howl({
+    src: ['assets/sounds/key-6.wav']
+  }),
+  7: new Howl({
+    src: ['assets/sounds/key-7.wav']
+  }),
+  8: new Howl({
+    src: ['assets/sounds/key-8.wav']
+  }),
+  9: new Howl({
+    src: ['assets/sounds/key-9.wav']
+  }),
+  10: new Howl({
+    src: ['assets/sounds/key-10.wav']
+  }),
+  11: new Howl({
+    src: ['assets/sounds/key-11.wav']
+  }),
+  12: new Howl({
+    src: ['assets/sounds/key-12.wav']
+  }),
+  13: new Howl({
+    src: ['assets/sounds/key-13.wav']
+  }),
+  14: new Howl({
+    src: ['assets/sounds/key-14.wav']
+  }),
+  15: new Howl({
+    src: ['assets/sounds/key-15.wav']
+  }),
+  16: new Howl({
+    src: ['assets/sounds/key-16.wav']
+  }),
+  17: new Howl({
+    src: ['assets/sounds/key-17.wav']
+  }),
+  18: new Howl({
+    src: ['assets/sounds/key-18.wav']
+  }),
+  19: new Howl({
+    src: ['assets/sounds/key-19.wav']
+  }),
+  20: new Howl({
+    src: ['assets/sounds/key-20.wav']
+  }),
+  21: new Howl({
+    src: ['assets/sounds/key-21.wav']
+  }),
+  22: new Howl({
+    src: ['assets/sounds/key-22.wav']
+  }),
+  23: new Howl({
+    src: ['assets/sounds/key-23.wav']
+  }),
+  24: new Howl({
+    src: ['assets/sounds/key-24.wav']
+  }),
+  25: new Howl({
+    src: ['assets/sounds/key-25.wav']
+  }),
+  26: new Howl({
+    src: ['assets/sounds/key-26.wav']
+  }),
+  27: new Howl({
+    src: ['assets/sounds/key-27.wav']
+  }),
+  28: new Howl({
+    src: ['assets/sounds/key-28.wav']
+  }),
+  29: new Howl({
+    src: ['assets/sounds/key-29.wav']
+  }),
+  30: new Howl({
+    src: ['assets/sounds/key-30.wav']
+  }),
+  31: new Howl({
+    src: ['assets/sounds/key-31.wav']
+  }),
+  32: new Howl({
+    src: ['assets/sounds/key-32.wav']
+  }),
+  33: new Howl({
+    src: ['assets/sounds/key-33.wav']
+  }),
+  34: new Howl({
+    src: ['assets/sounds/key-34.wav']
+  }),
+  35: new Howl({
+    src: ['assets/sounds/key-35.wav']
+  }),
+  36: new Howl({
+    src: ['assets/sounds/key-36.wav']
+  }),
+  37: new Howl({
+    src: ['assets/sounds/key-37.wav']
+  }),
+  38: new Howl({
+    src: ['assets/sounds/key-38.wav']
+  }),
+  39: new Howl({
+    src: ['assets/sounds/key-39.wav']
+  }),
+  40: new Howl({
+    src: ['assets/sounds/key-40.wav']
+  }),
+  41: new Howl({
+    src: ['assets/sounds/key-41.wav']
+  }),
+  42: new Howl({
+    src: ['assets/sounds/key-42.wav']
+  }),
+  43: new Howl({
+    src: ['assets/sounds/key-43.wav']
+  }),
+  44: new Howl({
+    src: ['assets/sounds/key-44.wav']
+  }),
+  45: new Howl({
+    src: ['assets/sounds/key-45.wav']
+  }),
+  46: new Howl({
+    src: ['assets/sounds/key-46.wav']
+  }),
+  47: new Howl({
+    src: ['assets/sounds/key-47.wav']
+  }),
+  48: new Howl({
+    src: ['assets/sounds/key-48.wav']
+  }),
+  49: new Howl({
+    src: ['assets/sounds/key-49.wav']
+  }),
+  50: new Howl({
+    src: ['assets/sounds/key-50.wav']
+  }),
+  51: new Howl({
+    src: ['assets/sounds/key-51.wav']
+  }),
+  52: new Howl({
+    src: ['assets/sounds/key-52.wav']
+  }),
+  53: new Howl({
+    src: ['assets/sounds/key-53.wav']
+  }),
+  54: new Howl({
+    src: ['assets/sounds/key-54.wav']
+  }),
+  55: new Howl({
+    src: ['assets/sounds/key-55.wav']
+  }),
+  56: new Howl({
+    src: ['assets/sounds/key-56.wav']
+  }),
+  57: new Howl({
+    src: ['assets/sounds/key-57.wav']
+  }),
+  58: new Howl({
+    src: ['assets/sounds/key-58.wav']
+  }),
+  59: new Howl({
+    src: ['assets/sounds/key-59.wav']
+  }),
+  60: new Howl({
+    src: ['assets/sounds/key-60.wav']
+  }),
+  61: new Howl({
+    src: ['assets/sounds/key-61.wav']
+  }),
+};
+
